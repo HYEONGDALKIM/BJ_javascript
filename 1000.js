a557d99c13b536ddc0c0a3cd0ff34db7904aafe2
HYEONGDALKIM <guddak@kakao.com>
2021-01-26 22:10:12 +0900

    Addition.file

---
+++ 1000.js
@@ -0,0 +1,6 @@
+var  fs  =  require ( 'fs' ) ; 
+var  input  =  fs . readFileSync ( 'data.txt' ) . toString ( ) . 분할 ( '' ) ;
+var  A  =  parseInt ( 입력 [ 0 ] ) ;
+var  B  =  parseInt ( 입력 [ 1 ] ) ;
+
+콘솔 . 로그 ( A  +  B ) ;
