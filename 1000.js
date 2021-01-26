ec456404fafb59114732775a701c3dd6786d16e7
HYEONGDALKIM <guddak@kakao.com>
2021-01-26 22:16:53 +0900

    plus_file

--- 1000.js
+++ 1000.js
@@ -3,6 +3,4 @@ var input = fs.readFileSync('data.txt').toString().split(' ');
 var A = parseInt(input[0]);
 var B = parseInt(input[1]);
 
-console.log(A + B);
-
->>>>>>> 2411a364a681d3b55cbb323723a4cafd386239f2
+console.log( A + B );
